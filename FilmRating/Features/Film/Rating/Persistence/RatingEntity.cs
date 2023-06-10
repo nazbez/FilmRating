4e756bf6db5c7f022525e3d5d4bd11890ad00ed6
dmytryG <dmytrygolub@gmail.com>
2023-06-10 23:37:56 +0300

    Added IS favourite in DB, added gitignore

--- FilmRating/Features/Film/Rating/Persistence/RatingEntity.cs
+++ FilmRating/Features/Film/Rating/Persistence/RatingEntity.cs
@@ -7,6 +7,7 @@ public class RatingEntity : IEntity<int>
 {
     public int Id { get; private set; }
     public int Rate { get; private set; }
+    public bool IsFavourite { get; private set; } = false;
     public int FilmId { get; private set; }
     public string UserId { get; private set; } = null!;
     public FilmEntity? Film { get; private set; }
---
+++ FilmRating/Persistence/Sql/Migrations/20230610202744_IsFavourite_feature.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FilmRating.Migrations
+{
+    /// <inheritdoc />
+    public partial class IsFavourite_feature : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFavourite",
+                table: "Rating",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFavourite",
+                table: "Rating");
+        }
+    }
+}
