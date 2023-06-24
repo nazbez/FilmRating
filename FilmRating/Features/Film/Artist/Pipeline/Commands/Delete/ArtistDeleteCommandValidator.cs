5e81f5a023c21637f94758b29ce46a345fd3e40c
Nazar Bezuhlyi <vlm.9a.nazar.bezuglyi@gmail.com>
2023-06-24 15:02:52 +0300

    Show related films in modal window when cannot delete artist

--- FilmRating/Features/Film/Artist/Pipeline/Commands/Delete/ArtistDeleteCommandValidator.cs
+++ FilmRating/Features/Film/Artist/Pipeline/Commands/Delete/ArtistDeleteCommandValidator.cs
@@ -1,5 +1,6 @@
 ﻿using FilmRating.Infrastructure.Repository;
 using FluentValidation;
+using FluentValidation.Results;
 using JetBrains.Annotations;
 
 namespace FilmRating.Features.Film.Artist;
@@ -7,26 +8,42 @@ namespace FilmRating.Features.Film.Artist;
 [UsedImplicitly]
 public class ArtistDeleteCommandValidator : AbstractValidator<ArtistDeleteCommand>
 {
+    private readonly ArtistExistsValidator artistExistsValidator;
+    private readonly IRepository<ArtistEntity, Guid> artistRepository;
+    
     public ArtistDeleteCommandValidator(
         ArtistExistsValidator artistExistsValidator,
         IRepository<ArtistEntity, Guid> artistRepository)
     {
+        this.artistExistsValidator = artistExistsValidator;
+        this.artistRepository = artistRepository;
+    }
+    
+    public override ValidationResult Validate(ValidationContext<ArtistDeleteCommand> context)
+    {
+        var artist = artistRepository.Find(
+                new ArtistGetByIdSpecification(context.InstanceToValidate.Id, withDirectorFilms: true, withActorFilms: true))
+            .First();
+
         RuleFor(cmd => cmd.Id)
             .SetValidator(artistExistsValidator)
             .DependentRules(() =>
             {
-                RuleFor(cmd => cmd.Id)
-                    .Must(id =>
-                    {
-                        var artist = artistRepository.Find(
-                                new ArtistGetByIdSpecification(id, withDirectorFilms: true, withActorFilms: true))
-                            .First();
-
-                        return !artist.DirectorFilms.Any() && !artist.ActorFilms.Any();
-                    })
-                    .WithMessage(cmd => ErrorMessage(cmd.Id));
+                RuleFor(a => a)
+                    .Must(_ => !artist.DirectorFilms.Any() && !artist.ActorFilms.Any())
+                    .WithMessage(cmd => ErrorMessage(artist.ActorFilms, artist.DirectorFilms));
             });
+        
+        return base.Validate(context);
     }
-    
-    private static string ErrorMessage(Guid id) => $"Cannot delete this artist. Artist has related films";
+
+    private static string ErrorMessage(
+        IEnumerable<FilmEntity> artistActorFilms,
+        IEnumerable<FilmEntity> artistDirectorFilms)
+    {
+        var films = artistActorFilms.Select(x => x.Title)
+            .Union(artistDirectorFilms.Select(x => x.Title));
+
+        return $"Cannot delete this artist. Artist has related films: {string.Join(", ", films)}";
+    }
 }
